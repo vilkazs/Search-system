c1057ca71ae9d4b1d15715930a6f2f5c138e2c2b
vilkazs <vilka_zs@mail.ru>
2023-04-20 18:01:05 +0300

    Added TD with type conversion

---
+++ Hello/TD_1_double.cpp
@@ -0,0 +1,12 @@
+﻿// Cчитать два числа и вывести их частное
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double x;
+    int y;
+    cin >> x >> y;
+    cout << x / (y + 0.0) << endl;
+}
---
+++ Hello/TD_2_double_to_int.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    double x, y;
+    cin >> x >> y;
+    cout << static_cast<int>(x + y) << endl; // считайте два числа, вывести целую часть их суммы
+}

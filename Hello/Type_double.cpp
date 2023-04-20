9374e1cb283db59502c7639188e489034c0ff805
vilkazs <vilka_zs@mail.ru>
2023-04-20 17:45:24 +0300

    Added Type_double

---
+++ Hello/Type_double.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x = 1;
+    int y = 2;
+    // Результат типа int и равен 0.
+    cout << x / y << endl;
+
+    // приведение типа к double. Результат типа double и 0.5.
+    cout << x / (y + 0.0) << endl;
+}

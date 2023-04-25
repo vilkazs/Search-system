8faacafc6230fcb582840eb6496e455f3dc986b6
vilkazs <vilka_zs@mail.ru>
2023-04-25 18:31:53 +0300

    Added TD_length

---
+++ Hello/TD_10_length.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+
+    string text = to_string(num) + " animals found"s;
+    cout << text << endl;
+    return 0;
+}
---
+++ Hello/TD_11_string.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int int1, int2;
+    // Считать два целых числа
+    cin >> int1 >> int2;
+    // Вывести произведение их суммы на количество чисел в записи суммы
+    int sum = int1 + int2;
+    string length = to_string(sum);
+    cout << sum * length.size();
+}

a5b910999c7a5c917fe724c264e511bd377b02d6
vilkazs <vilka_zs@mail.ru>
2023-05-08 18:09:59 +0300

    Added function for palindrom

---
+++ Functions/function_1_palindrom.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+void IsPalindrome(string s) {
+    
+    string word;
+    
+    for(int i = s.size()-1; i >= 0; i--) {
+        word = word + s[i];
+    }
+
+    if (s == word)  {
+        cout << "1" << endl;
+    }
+    else {
+        cout << "0" << endl;
+    } 
+    
+}
+
+int main() {
+    IsPalindrome(""s);
+    IsPalindrome("h"s);
+    IsPalindrome("tree"s);
+    IsPalindrome("mom"s);
+}
---
+++ Functions/function_2_palindrom.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+void IsPalindrome(string s) {
+    int mes = 1;
+    for (int i = 0; i < s.size() / 2; ++i) {
+        if (s[i] != s[s.size() - i - 1]) {
+            mes = 0;
+			break;
+        }
+    }
+    cout << mes << endl;
+}
+
+int main() {
+    IsPalindrome(""s);
+    IsPalindrome("h"s);
+    IsPalindrome("deep"s);
+    IsPalindrome("mom"s);
+    
+    return 0;
+}

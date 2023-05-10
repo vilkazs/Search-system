1a8c52c7593fbdb5da9e968f168d0409698dc7c4
vilkazs <vilka_zs@mail.ru>
2023-05-10 18:29:15 +0300

    Added functions

---
+++ Functions/function_3_make_vector.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> SplitIntoWords(string text) {
+    
+    vector<string> words;
+    string word;
+    
+     for (char c : text) {
+        if (c == ' ') {
+            if (!word.empty()) {
+                words.push_back(word);
+                word.clear();
+            }
+        } else {
+            word += c;
+        }
+    }
+    if (!word.empty()) {
+        words.push_back(word);
+    }
+    
+    return words;
+}
+
+int main() {
+	string query;
+	getline(cin, query);
+		
+    SplitIntoWords(query);
+    
+    /*for (string word : SplitIntoWords(query)) {
+        cout << '[' << word << ']' << endl;
+    }*/
+    
+    return 0;
+}
---
+++ Functions/function_4.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int Factorial(int x) {
+    
+    if (x <= 0) {
+        return 1;
+    } else {
+        int result = 1;
+        for (int i = 1; i <= x; ++i) {
+            result *= i;
+        }
+        return result;
+    }
+    
+}
+
+int main () {
+    int x;
+    cin >> x;
+    Factorial(x);
+}

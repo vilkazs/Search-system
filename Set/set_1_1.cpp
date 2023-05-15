d85001af972fa051bbcd52f1cbe9ec9ba536c203
vilkazs <vilka_zs@mail.ru>
2023-05-15 14:30:38 +0200

    Added Set

---
+++ Set/set_1_1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+#include <set>
+
+using namespace std;
+
+int main() {
+    // считываем число строк
+    int N;
+    cin >> N;
+    // создаём множество
+    set<string> words;
+    // заполняем его строками и выводим размер
+    string word;
+    for (int i = 0; i < N; ++i) {
+        string word;
+        cin >> word;
+        words.insert(word);
+    }
+    cout << words.size() << endl;
+    return 0;
+}
---
+++ Set/set_1_2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+#include <set>
+
+using namespace std;
+
+int main() {
+    // множество
+    set<string> words;
+    // заполняем его строками и выводим размер
+    string word;
+    while (cin >> word) {
+        if (word == "end"s) {
+            break;
+        }
+        words.insert(word);
+    }
+    cout << words.size() << endl;
+    return 0;
+}

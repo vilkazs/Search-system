548435341f4a966813e69c3d287403077ab47eb1
vilkazs <vilka_zs@mail.ru>
2023-06-06 16:49:57 +0200

    Added const links

---
+++ Deep copying, links, constancy/links_1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+ 
+using namespace std;
+
+void UpdateIfGreater(int limit, int& value) {
+	if (limit > value) {
+        value = limit;
+    }
+}
+
+/*
+Текущее максимальное расстояние лежит в целочисленной переменной max_distance. Каждое новое свидетельство — это целое число с расстоянием. И оно может увеличить max_distance.
+
+Написать функцию UpdateIfGreater, принимающую два целочисленных аргумента: limit и value. Если limit окажется больше value, пусть функция запишет в value значение параметра limit. Нужно, чтобы изменение параметра value было видно на вызывающей стороне. Возвращать и выводить функция ничего не должна. 
+
+Передать limit по значению (тип int), а value — по ссылке (тип int&). Внутри функции записать в value новое значение, если limit больше value.
+
+---Example---
+int max_distance = 26;
+UpdateIfGreater(25, max_distance);
+cout << max_distance << endl;  // выведет 26, так как 25 не больше 26
+UpdateIfGreater(40, max_distance);
+cout << max_distance << endl;  // выведет 40, так как 40 больше 26 
+*/
---
+++ Deep copying, links, constancy/links_2.cpp
@@ -0,0 +1,27 @@
+﻿#include <vector>
+#include <string>
+
+using namespace std;
+
+void MoveStrings(vector<string>& sourse, vector<string>& destination) {
+    for (string& word : sourse) {
+        destination.push_back(word);
+    }
+    sourse.clear(); 
+}
+
+/*
+---TD---
+Написать функцию MoveStrings. Она принимает векторы строк source и destination и дописывает все строки из первого вектора в конец второго. После выполнения функции вектор source должен стать пустым.
+
+
+---clear---
+Чтобы очистить содержимое вектора, вызовите у него метод clear:
+string query = "how to learn c++"s;
+vector<string> query_words = SplitIntoWords(query);
+query_words.clear();
+// Теперь вектор query_words пуст. Метод empty вернёт true, метод size — 0. 
+
+---
+Принять векторы строк source и destination по ссылке. Дописать строки в destination с помощью цикла и метода push_back. Перед выходом из функции очистить source методом clear.
+*/
